9d2588d82cdb481675edf753e398c555faa55a5c
kurt_ <kurt_@DESKTOP-GL14941>
2023-03-14 16:46:50 +0300

    Add project files.

---
+++ RazorLibrary/Models/TodoModel.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace RazorLibrary.Models
+{
+    public class TodoModel
+    {
+        public string TodoItem { get; set; }
+        public bool IsComplete { get; set; }
+    }
+    
+}
---
+++ TebMauiApp/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TebMauiApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
